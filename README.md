8b63740bfaefbba6915d5c6106df8b1a282e237f
Sadie Cinader <sadie@arthur-639.local>
2016-09-18 13:25:21 -0700

    Setup works now.
    added lint.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Alexa-Birthday-Calendar
 A Birthday Calendar for Amazon Alexa
+
+SetupState
+- What is your name?
+- When is your Birthday?
+
+Query State
+- Whose birthday is next
+- Is there a birthday today
+- When is [name]'s birthday
+- How old is Name
+Add a new birthday (for [name])
+- whose calendar is this?
+
+Entry State
+- [name]'s birthday
+- [date]
+
+
+Stored Attributes
+
+this.attributes['owner'] = string Name// the name of the owner of the calendar
+this.attributes['birthdays'] = {
+    name: birthday
+}
---
+++ src/index.js
@@ -0,0 +1,216 @@
+const Alexa = require('alexa-sdk');
+const moment = require('moment');
+
+const appId = 'amzn1.ask.skill.65751814-8f39-45ed-b452-e1d42a223d02';
+
+const imageObj = {
+  smallImageUrl: 'https://imgs.xkcd.com/comics/standards.png',
+  largeImageUrl: 'https://imgs.xkcd.com/comics/standards.png',
+};
+
+const states = {
+  SETUPMODE: '_SETUPMODE', // Add your name & birthday
+  QUERYMODE: '_QUERYMODE', // Ask about birthdays
+  ENTRYMODE: '_ENTRYMODE',  // Enter Birthdays
+};
+
+const addBirthday = function addBirthday() {
+  if (!this.attributes.birthdays) {
+    this.attributes.birthdays = {};
+  }
+  const currentlyAdding = this.attributes.currentlyAdding;
+  const name = currentlyAdding.name;
+  const birthdate = currentlyAdding.birthdate;
+
+  delete this.attributes.currentlyAdding;
+
+  this.attributes.birthdays[name] = birthdate;
+};
+
+const enterNameIntent = function enterNameIntent() {
+  const name = this.event.request.intent.slots.EnteredName.value;
+  this.attributes.currentlyAdding = { name };
+  this.emit(':ask', `Ok, I heard ${name}, is that correct?`, `Say yes if the name, ${name} is correct, or no to reenter.`);
+};
+
+const enterBirthdateIntent = function enterBirthdateIntent() {
+  const birthdateString = this.event.request.intent.slots.EnteredBirthdate.value;
+  const birthdate = moment(birthdateString);
+  const now = moment();
+  if (!this.attributes.owner) {
+    this.handler.state = states.SETUPMODE;
+    this.emit(':ask',
+      'oh my, this is embarassing, but I don\'t know who you are yet.  Please say your name',
+      'say your name or quit to exit');
+  }
+
+  if (birthdate.year() >= now.year()) {
+    this.emit(':ask', 'You need to include the year you were born.', 'Say your birthday including the year');
+  } else {
+    this.attributes.currentlyAdding.birthdate = birthdateString;
+    this.emit(':ask', `Ok, ${this.attributes.owner}, ` +
+      `I heard  <say-as interpret-as="date" format="mdy">${birthdate.format('M/D/Y')}</say-as>, is that correct?`,
+      `Say yes if the date <say-as interpret-as="date" format="mdy">${birthdate.format('M/D/Y')}</say-as> is correct, or no to reenter your birthday`);
+  }
+};
+
+const newSessionHandlers = {
+  // This will short-cut any incoming intent or launch requests and route them to this handler.
+  NewSession: function newSession() {
+    const requiresSetup =
+    // is there any stored state?
+    Object.keys(this.attributes).length === 0
+      // is there an owner?
+      || !this.attributes.owner
+      // are there any birthdays?
+      || !this.attributes.birthdays
+      // is there a birthday for the owner?
+      || !this.attributes.birthdays[this.attributes.owner];
+
+    if (requiresSetup) { // Check if it's the first time the skill has been invoked
+      this.attributes.endedSessionCount = 0;
+      this.handler.state = states.SETUPMODE;
+      this.emit(':ask', 'Welcome to your Birthday Calendar. Let\'s start by setting you up. First, what is your name?',
+            'Say your name or quit to exit.');
+    } else {
+      this.handler.state = '';
+      this.emit(':ask', `Welcome to ${this.attributes.owner}'s Birthday Calendar.  ` +
+        'Say \'ask\' to lookup birthdays or \'enter\' to add people to your calendar',
+        'Say \'ask\' to lookup birthdays or \'enter\' to add people to your calendar');
+    }
+  },
+
+  QueryModeIntent() {
+    this.handler.state = states.QUERYMODE;
+    this.emit(':ask',
+            'What would you like to ask your birthday calendar?',
+            // add randome name function of known names
+            'You can ask, how old someone is, when is a birthday for a person, or whose birthday is next'
+        );
+  },
+
+  EntryModeIntent() {
+    this.handler.state = states.ENTRYMODE;
+    this.emitWithState('AddNameIntent');
+  },
+};
+
+const endSessionHandlers = {
+  SessionEndedRequest() {
+    console.log('session ended!');
+    delete this.attributes.currentlyAdding;
+    this.attributes.endedSessionCount += 1;
+    this.emit(':saveState', true); // Be sure to call :saveState to persist your session attributes in DynamoDB
+  },
+};
+
+const unhandeledHandlers = {
+  Unhandled() {
+    console.log('this is a generic undhandled');
+    this.emit(':ask', 'huh?  I don\'t understand', 'there should be thre re-ask...');
+  },
+};
+
+const setupModeHandlers = Alexa.CreateStateHandler(states.SETUPMODE, {
+  NewSession() {
+    this.handler.state = '';
+    this.emitWithState('NewSession'); // Equivalent to the Start Mode NewSession handler
+  },
+
+  AddNameIntent: enterNameIntent,
+  AddBirthdateIntent: enterBirthdateIntent,
+
+  'AMAZON.NoIntent': function () {
+    if (!this.attributes.currentlyAdding) {
+            // TODO: this shouldn't happen and is an error.
+            // what to do with an erro?
+            // should ask to enter owner name....
+    }
+
+    if (this.attributes.currentlyAdding.birthdate) {
+            // if we have a birthday in the currently adding object,
+            // and we're in the no intent, then we know that the user
+            // said no when we asked them to verify the birthday.
+      delete this.attributes.currentlyAdding.birthdate;
+      this.emit(':ask', `Ok, ${this.attributes.owner}, what is your birthday?`, 'Please say your birthday, or exit to quit');
+    } else {
+            // if we don't have a birthday, we should have the name but
+            // since we're in the no block, they said that we got it wrong.
+            // so ask them to say it again.
+      delete this.attributes.currentlyAdding;
+      this.emit(':ask', 'Ok, please say your name again.', 'Please say your name, or exit to quit');
+    }
+  },
+
+  'AMAZON.YesIntent': function () {
+    if (!this.attributes.currentlyAdding) {
+            // TODO: this shouldn't happen and is an error.
+            // what to do with an erro?
+            // should ask to enter owner name....
+    }
+    if (!this.attributes.currentlyAdding.birthdate) {
+    // ok, we have the owner name right!  Add it to the attributes
+      const name = this.attributes.currentlyAdding.name;
+    // add the owner to the attributes so we'll always no whose calendar it is!
+      this.attributes.owner = name;
+      // I don't think this intent will actually have a card, but just adding as a sample
+      this.emit(
+        ':askWithCard',
+        `Ok, hi ${name}.  What is your birthday?`,
+        'Say your birthday including year',
+        'This is the card title',
+        'This is what the card says (the content)',
+        imageObj);
+    } else {
+      // this is so lame.  Creating a bound function and calling it.
+      // this is the way to bind "this".  I'd rather just send this
+      // as an argument, but eslint airbnb style doesn't like it.
+      addBirthday.bind(this)();
+      this.state = '';
+      this.emit(
+                ':ask',
+                `Ok, ${this.attributes.owner}, would you like to look up birthdays, or add more names?`,
+                'Say look up to look up birthdays, or say add to add more names to the calendar.');
+    }
+  },
+
+  'AMAZON.HelpIntent': function () {
+    this.emit(':ask', 'I am thinking of a number between zero and one hundred, try to guess and I will tell you' +
+            ' if it is higher or lower.', 'Try saying a number.');
+  },
+
+  SessionEndedRequest() {
+    this.handler.state = '';
+    this.emitWithState('SessionEndedRequest');
+  },
+
+  Unhandled() {
+    this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+  },
+});
+
+const entryModeHandlers = Alexa.CreateStateHandler(states.ENTRYMODE, {
+
+});
+
+const queryModeHandlers = Alexa.CreateStateHandler(states.QUERYMODE, {
+  Unhandled() {
+    this.emit(':ask', 'Sorry, We haven\'t made this part yet!.', 'Try saying a number.');
+  },
+});
+
+
+exports.handler = (event, context, callback) => { // eslint-disable-line no-unused-vars
+  const alexa = Alexa.handler(event, context);
+  alexa.appId = appId;
+  alexa.dynamoDBTableName = 'BirthdayCalendar';
+  alexa.registerHandlers(
+        unhandeledHandlers,
+        newSessionHandlers,
+        endSessionHandlers,
+        setupModeHandlers,
+        entryModeHandlers,
+        queryModeHandlers
+    );
+  alexa.execute();
+};
